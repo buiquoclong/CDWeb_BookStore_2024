333ce280e1901a2698d39d36d7fde555fb5e2b80
Bùi Quốc Long <20130314@st.hcmuaf.edu.vn>
2024-05-13 23:37:40 +0700

    list book with filter, hot book, discount and new book

--- src/main/java/com/cdweb/bookstore/service/IBookService.java
+++ src/main/java/com/cdweb/bookstore/service/IBookService.java
@@ -1,6 +1,7 @@
 package com.cdweb.bookstore.service;
 
 import com.cdweb.bookstore.dto.BookDTO;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -11,4 +12,33 @@ public interface IBookService {
     public BookDTO findById(int id);
 
     public List<BookDTO> findByCategoryIdAnQuantityGreaterThan(int categoryId, int quantity);
+    public List<BookDTO> findByCategoryCode(String categoryCode, Pageable pageable);
+
+    public List<BookDTO> findAllByAuthorCode(String code, Pageable pageable);
+    public List<BookDTO> findAll(Pageable pageable);
+    public List<BookDTO> findAllHotBook(boolean isActive, boolean isHot, Pageable pageable);
+
+    public List<BookDTO> findAllNewBook(boolean isActive, boolean isNew, Pageable pageable);
+    public List<BookDTO> findAllContainTitle(String title, Pageable pageable);
+    public List<BookDTO> findByPriceBetween(int from, int to, Pageable pageable);
+    public List<BookDTO> findByPriceGreaterThan(int from, Pageable pageable);
+
+    //count
+    public int countByCategory(String code);
+
+    public int countByAuthorCode(String code);
+
+    public int countAllByActive(boolean isActive);
+
+    public int countAllByHot(boolean isActive, boolean isHot);
+
+    public int countAllByNews(boolean isActive, boolean isNew);
+
+    public int countAllByTitleContains(String titles);
+
+    public int countAllByPriceBetween(int from, int to);
+
+    public int countAllByPriceGreaterThan(int from);
+    public List<BookDTO> findAllByActiveAndDicount(boolean active, double discountFrom, double discountTo, Pageable pageable);
+    public int countAllByActiveAndDiscount(boolean active, double discountFrom, double discountTo);
 }
--- src/main/java/com/cdweb/bookstore/service/impl/BookServiceImp.java
+++ src/main/java/com/cdweb/bookstore/service/impl/BookServiceImp.java
@@ -6,6 +6,7 @@ import com.cdweb.bookstore.entities.BookEntity;
 import com.cdweb.bookstore.repository.BookRepository;
 import com.cdweb.bookstore.service.IBookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -49,6 +50,133 @@ public class BookServiceImp implements IBookService {
             results.add(bookConverter.toDTO(b));
         }
         return results;
+    }
+    @Override
+    public List<BookDTO> findByCategoryCode(String categoryCode, Pageable pageable) {
+        List<BookDTO> results = new ArrayList<>();
+        for (BookEntity b : bookRepo.findByCategoryCode(categoryCode, pageable).getContent()) {
+            results.add(bookConverter.toDTO(b));
+        }
+        return results;
+    }
+
+    @Override
+    public List<BookDTO> findAllByAuthorCode(String code, Pageable pageable) {
+        List<BookDTO> results = new ArrayList<>();
+        for (BookEntity b : bookRepo.findAllByAuthorAuthorCode(code, pageable).getContent()) {
+            results.add(bookConverter.toDTO(b));
+        }
+        return results;
+    }
+
+    @Override
+    public List<BookDTO> findAll(Pageable pageable) {
+        List<BookDTO> results = new ArrayList<>();
+
+        //hàm findAll(pageable) sẽ trả về Page<BookEntity>, để chuyển Page thành List thì dùng hàm getContent()
+        for (BookEntity b : bookRepo.findAll(pageable).getContent()) {
+            results.add(bookConverter.toDTO(b));
+        }
+        return results;
+    }
+
+    @Override
+    public List<BookDTO> findAllHotBook(boolean isActive, boolean isHot, Pageable pageable) {
+        List<BookDTO> results = new ArrayList<>();
+        for (BookEntity b : bookRepo.findAllByActiveAndHot(isActive, isHot, pageable).getContent()) {
+            results.add(bookConverter.toDTO(b));
+        }
+        return results;
+    }
+
+    @Override
+    public List<BookDTO> findAllNewBook(boolean isActive, boolean isNew, Pageable pageable) {
+        List<BookDTO> results = new ArrayList<>();
+        for (BookEntity b : bookRepo.findAllByActiveAndNews(isActive, isNew, pageable).getContent()) {
+            results.add(bookConverter.toDTO(b));
+        }
+        return results;
+    }
+
+    @Override
+    public List<BookDTO> findAllContainTitle(String title, Pageable pageable) {
+        List<BookDTO> results = new ArrayList<>();
+        for (BookEntity b : bookRepo.findAllByTitleContains(title, pageable).getContent()) {
+            results.add(bookConverter.toDTO(b));
+        }
+        return results;
+    }
+
+    @Override
+    public List<BookDTO> findByPriceBetween(int from, int to, Pageable pageable) {
+        List<BookDTO> results = new ArrayList<>();
+        for (BookEntity b : bookRepo.findAllByPriceBetween(from, to, pageable).getContent()) {
+            results.add(bookConverter.toDTO(b));
+        }
+        return results;
+    }
+
+    @Override
+    public List<BookDTO> findByPriceGreaterThan(int from, Pageable pageable) {
+        List<BookDTO> results = new ArrayList<>();
+        for (BookEntity b : bookRepo.findAllByPriceGreaterThan(from, pageable).getContent()) {
+            results.add(bookConverter.toDTO(b));
+        }
+        return results;
+    }
+
+    @Override
+    public int countByCategory(String code) {
+        return bookRepo.countAllByCategoryCode(code);
+    }
+
+    @Override
+    public int countByAuthorCode(String code) {
+        return bookRepo.countAllByAuthorAuthorCode(code);
+    }
+
+    @Override
+    public int countAllByActive(boolean isActive) {
+        return bookRepo.countAllByActive(isActive);
+    }
+
+    @Override
+    public int countAllByHot(boolean isActive, boolean isHot) {
+        return bookRepo.countAllByActiveAndHot(isActive, isHot);
+    }
+
+    @Override
+    public int countAllByNews(boolean isActive, boolean isNew) {
+        return bookRepo.countAllByActiveAndNews(isActive, isNew);
+    }
+
+    @Override
+    public int countAllByTitleContains(String title) {
+        return bookRepo.countAllByTitleContains(title);
+    }
+
+    @Override
+    public int countAllByPriceBetween(int from, int to) {
+        return bookRepo.countAllByPriceBetween(from, to);
+    }
+
+    @Override
+    public int countAllByPriceGreaterThan(int from) {
+        return bookRepo.countAllByPriceGreaterThan(from);
+    }
+
+    @Override
+    public List<BookDTO> findAllByActiveAndDicount(boolean active, double from, double to, Pageable pageable) {
+        List<BookDTO> results = new ArrayList<>();
+        for (BookEntity b : bookRepo.findAllByActiveAndDiscountPercentBetween(true, from, to, pageable).getContent()) {
+            results.add(bookConverter.toDTO(b));
+        }
+        return results;
+    }
+
+    @Override
+    public int countAllByActiveAndDiscount(boolean active, double from, double to) {
+        return bookRepo.countAllByActiveAndDiscountPercentBetween(active, from, to);
     }
 
 }
